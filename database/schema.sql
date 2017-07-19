6bdb4734b1bf6006a42088378af12680f452963b
Keith Oppel <mistakeets@gmail.com>
2017-07-19 14:26:06 -0700

    WIP: update seed file to handle associative SQL transaction

--- database/schema.sql
+++ database/schema.sql
@@ -4,18 +4,19 @@ CREATE DATABASE bookstore;
 \c bookstore;
 
 CREATE TABLE book (
-  id SERIAL PRIMARY KEY,
+  book_id SERIAL PRIMARY KEY,
   title VARCHAR(100),
   genre VARCHAR(100)
 );
 
 CREATE TABLE author (
-  id SERIAL PRIMARY KEY,
+  author_id SERIAL PRIMARY KEY,
   first_name VARCHAR(100),
   last_name VARCHAR(100)
 );
 
 CREATE TABLE book_author (
-  book_id INTEGER REFERENCES book(id),
-  author_id INTEGER REFERENCES author(id)
+  book_id INTEGER REFERENCES book(book_id),
+  author_id INTEGER REFERENCES author(author_id),
+  PRIMARY KEY (book_id, author_id)
 );
