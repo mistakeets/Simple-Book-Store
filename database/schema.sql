f2456c30fec8fca2fdf6e1f470b4c6428c0301f0
Keith Oppel <mistakeets@gmail.com>
2017-07-18 17:06:11 -0700

    adds initial idea for seeding data

--- database/schema.sql
+++ database/schema.sql
@@ -5,25 +5,17 @@ CREATE DATABASE bookstore;
 
 CREATE TABLE book (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(100)
+  title VARCHAR(100),
+  genre VARCHAR(100)
 );
 
 CREATE TABLE author (
   id SERIAL PRIMARY KEY,
-  first_name VARCHAR (50),
-  last_name VARCHAR(50)
-);
-
-CREATE TABLE genre (
-  id SERIAL PRIMARY KEY,
-  genre_name VARCHAR (50)
+  first_name VARCHAR(100),
+  last_name VARCHAR(100)
 );
 
 CREATE TABLE book_author (
   book_id INTEGER REFERENCES book(id),
   author_id INTEGER REFERENCES author(id)
 );
-
-ALTER TABLE book ADD COLUMN genre INTEGER REFERENCES genre (id);
-
-
