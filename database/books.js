c108f2534e4ba0ad03fe3ae2d9dde6690359998b
Keith Oppel <mistakeets@gmail.com>
2017-07-21 16:09:27 -0700

    un comment third insert for addBook query

--- database/books.js
+++ database/books.js
@@ -3,13 +3,13 @@ const db = require('./db')
 const addBook = (title, firstName, lastName, genre) => {
   return db.tx(t => {
       return t.batch([
-        t.one('INSERT INTO book(title, genre) VALUES($1, $2) RETURNING id', [id]),
-        t.one('INSERT INTO author(first_name, last_name) VALUES($1, $2) RETURNING id', [id])
+        t.one('INSERT INTO book(title, genre) VALUES($1, $2) RETURNING id', [title, genre]),
+        t.one('INSERT INTO author(first_name, last_name) VALUES($1, $2) RETURNING id', [firstName, lastName])
       ])
     })
     .then(data => {
       console.log('DO WE HAVE DATA??', data)
-        //return db.oneOrNone('INSERT INTO book_author(book_id, author_id) VALUES (data[0].id, data[1].id)')
+      return db.oneOrNone('INSERT INTO book_author(book_id, author_id) VALUES (data[0].id, data[1].id)')
     })
 }
 
