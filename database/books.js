c12b766c85ac638f24832d275abee48706340482
Keith Oppel <mistakeets@gmail.com>
2017-07-21 16:03:19 -0700

    WIP: make route work in order to add books to db

---
+++ database/books.js
@@ -0,0 +1,20 @@
+const db = require('./db')
+
+const addBook = (title, firstName, lastName, genre) => {
+  return db.tx(t => {
+      return t.batch([
+        t.one('INSERT INTO book(title, genre) VALUES($1, $2) RETURNING id', [id]),
+        t.one('INSERT INTO author(first_name, last_name) VALUES($1, $2) RETURNING id', [id])
+      ])
+    })
+    .then(data => {
+      console.log('DO WE HAVE DATA??', data)
+        //return db.oneOrNone('INSERT INTO book_author(book_id, author_id) VALUES (data[0].id, data[1].id)')
+    })
+}
+
+
+
+module.exports = {
+  addBook
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
+const books = require('../../database/books')
+const pgPromise = require('pg-promise')
 
 router.get('/', (request, response) => {
   response.render('index')
@@ -10,6 +12,14 @@ router.get('/search', (request, response) => {
 
 router.get('/admin', (request, response) => {
   response.render('admin')
+})
+
+router.post('/addBook', (request, response, done) => {
+  books.addBook(request.body.name)
+    .then(() => {
+      response.redirect('/admin')
+      done()
+    })
 })
 
 module.exports = router
