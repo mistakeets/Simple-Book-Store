ccb6a4d04b0c402b2591fe407127408573a74737
Keith Oppel <mistakeets@gmail.com>
2017-07-20 18:15:40 -0700

    refactors seed sql to use function for seeding book_author table

--- database/seed.sql
+++ database/seed.sql
@@ -1,6 +1,7 @@
 \c bookstore
 
 CREATE TEMPORARY TABLE temp_table (
+  id SERIAL PRIMARY KEY,
   title VARCHAR(100),
   first_name VARCHAR(100),
   last_name VARCHAR(100),
@@ -10,16 +11,34 @@ CREATE TEMPORARY TABLE temp_table (
 COPY temp_table(title,last_name,first_name, genre)
 FROM '/Users/koppel/Documents/LG/lgProjects/Simple-Book-Store/database/books.csv' DELIMITER ',' CSV HEADER;
 
-START TRANSACTION 
+-- INSERT INTO book(title, genre) SELECT title,genre FROM temp_table;
 
-DECLARE bookKey INTEGER, authorKey INTEGER;
+-- INSERT INTO author(first_name, last_name) SELECT first_name, last_name FROM temp_table GROUP BY first_name, last_name;
 
-INSERT INTO book(title, genre) SELECT title,genre FROM temp_table RETURNING id INTO bookKey;
+-- INSERT INTO book_author(book_id, author_id) SELECT * FROM book b inner join temp_table t on b.title = tt.title inner join author a on a.first_name = tt.author;
 
-INSERT INTO author(first_name, last_name) SELECT first_name, last_name FROM temp_table GROUP BY first_name, last_name RETURNING id INTO authorKey;
+-- DROP TABLE temp_table;
 
-INSERT INTO book_author(book_id, author_id) VALUES (bookKey, authorKey);
+CREATE FUNCTION pg_temp.foo() returns void AS $$
+DECLARE
+  temp_table_row RECORD;
+  a_book RECORD;
+  a_author RECORD;
+  existing_author RECORD;
+BEGIN
+ FOR temp_table_row IN select * from temp_table LOOP   
+   INSERT INTO book(title, genre) values(temp_table_row.title, temp_table_row.genre) returning * INTO a_book;
+   SELECT * INTO a_author from author where first_name = temp_table_row.first_name and last_name = temp_table_row.last_name;
+   IF NOT found THEN 
+     INSERT INTO author(first_name, last_name) values(temp_table_row.first_name, temp_table_row.last_name) returning * INTO a_author;
+   END IF;
+   INSERT INTO book_author(book_id, author_id) values(a_book.id, a_author.id);   
+ END LOOP;
+ return;
+END;
+$$ LANGUAGE plpgsql;
 
-COMMIT
+select pg_temp.foo();
 
-DROP TABLE temp_table;
+select count(*) from author;
+select count(*) from book;
