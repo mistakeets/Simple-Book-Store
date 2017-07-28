611b52f9c30aaa9e09b2f9073ea5c9d1e4852f6e
Keith Oppel <mistakeets@gmail.com>
2017-07-28 16:59:07 -0700

    WIP: adds initial code for search feature

--- server/routes/index.js
+++ server/routes/index.js
@@ -27,4 +27,17 @@ router.post('/addBook', (request, response, done) => {
     })
 })
 
+router.post('/findByTitle', (request, response, done) => {
+  const title = request.body.title
+  books.findByTitle(title)
+    .then(() => {
+      response.redirect('/searchResults')
+      done()
+    })
+})
+
+router.get('/searchResults', (request, response) => {
+  response.render('searchResults')
+})
+
 module.exports = router
